7dab80109c06db3070a5426088164d7ff3247ad9
Abraham Isaac Durán <aid.mest@gmail.com>
2020-11-20 18:54:59 -0400

    Remove unused function and usings

--- FlypackBot/FlypackScrapper.cs
+++ FlypackBot/FlypackScrapper.cs
@@ -1,9 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using FlypackBot.Models;
@@ -89,23 +87,6 @@ namespace FlypackBot
             }
 
             return builder.ToString();
-        }
-
-        private async Task<List<Cookie>> GetCookies(string url)
-        {
-            var cookieContainer = new CookieContainer();
-            var uri = new Uri(url);
-            using (var httpClientHandler = new HttpClientHandler
-            {
-                CookieContainer = cookieContainer
-            })
-            {
-                using (var httpClient = new HttpClient(httpClientHandler))
-                {
-                    await httpClient.GetAsync(uri);
-                    return cookieContainer.GetCookies(uri).Cast<Cookie>().ToList();
-                }
-            }
         }
     }
 }
